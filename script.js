e6d1874d1e6cbd0b8a11e18acf19582c590a57c9
Pedro Almeida <pedroh_7@hotmail.com>
2023-02-19 15:45:47 -0300

    Tweak playRound return and add game function

--- script.js
+++ script.js
@@ -22,19 +22,62 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection === computerSelection) {
-        return "Tie!";
+        // Tie!
+        return 0;
     } else if ((playerSelection === "rock" && computerSelection === "scissors")
             || (playerSelection === "scissors" && computerSelection === "paper")
             || (playerSelection === "paper" && computerSelection === "rock")) {
 
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
+        // Player wins!
+        return 1;
 
     } else if ((playerSelection === "rock" && computerSelection === "paper")
             || (playerSelection === "scissors" && computerSelection === "rock")
             || (playerSelection === "paper" && computerSelection === "scissors")) {
 
-        return `You Lose! ${playerSelection} loses to ${computerSelection}`;
+        // Computer wins!
+        return 2;
     } else {
-        return "Oops! Something went wrong!"
+        return 3;
     }
+}
+
+
+
+function game() {
+    let playerChoice = "";
+    let computerChoice = "";
+
+    let playerWins = 0;
+    let computerWins = 0;
+
+    for (let i = 0; i < 5; i++) {
+        playerChoice = prompt("Choose your weapon!\nRock, paper or scissors");
+        computerChoice = getComputerChoice();
+
+        let roundResult = playRound(playerChoice, computerChoice);
+
+        switch (roundResult) {
+            case 0:
+                console.log("Tie!");
+                break;
+            
+            case 1:
+                console.log(`You win! ${playerChoice} beats ${computerChoice}`);
+                playerWins++;
+                break;
+                
+            case 2:
+                console.log(`You lose! ${playerChoice} loses to ${computerChoice}`);
+                computerWins++;
+                break;
+            
+            case 3:
+                console.log(`Oops, something went wrong!\nMaybe ${playerChoice} isn't a option.`);
+                i--;
+                break;
+        }
+    }
+
+
 }
